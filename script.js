118d1b4bf18135a730548e0628d66b4629bceb06
thaistondin <thais.tondin@escola.pr.gov.br>
2026-08-04 21:00:28 -0300

    .

--- script.js
+++ script.js
@@ -22,7 +22,7 @@
     function mudaTema() {
         const corpoPagina = document.body;
         if (corpoPagina.classList.contains("tema-escuro")) {
-            corpoPagina.classList.remoe("tema-escuro");
+            corpoPagina.classList.remove("tema-escuro");
         } else {
             corpoPagina.classList.add("tema-escuro");
         }
